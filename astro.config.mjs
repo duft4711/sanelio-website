5bc8e7f38da2148e9097bd07487f41d9c1c4fc15
Prime <prime@local.invalid>
2026-07-28 15:43:32 +0000

    Prepare gated GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://sanelio.de",
   output: "static",
   trailingSlash: "always",
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        ![
+          "https://sanelio.de/datenschutz/",
+          "https://sanelio.de/impressum/",
+          "https://sanelio.de/kontakt/",
+          "https://sanelio.de/prime/entstehung/",
+          "https://sanelio.de/prime/prinzipien-und-arbeitsweise/",
+          "https://sanelio.de/prime/weiterentwicklung/",
+        ].includes(page),
+    }),
+  ],
 });
